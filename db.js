414645e36d37d0bfa4807e49dd0028478f20b3d0
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-09-15 01:35:21 -0500

    conexión a la db con dotenv

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const { connect } = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const db = await connect("mongodb://localhost/tasksdb");
+    console.log();
+    await connect(process.env.MONGODB_URI);
     console.log("Mongodb Connected");
   } catch (error) {
     console.error(error);
